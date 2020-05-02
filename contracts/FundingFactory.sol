931f5312b6bb267e74696706700e86d2185278c0
Leon Prouger <leonprou@gmail.com>
2020-05-02 14:43:41 +0300

    Make FundingFactory upgradable

--- contracts/FundingFactory.sol
+++ contracts/FundingFactory.sol
@@ -1,19 +1,21 @@
 
 pragma solidity ^0.5.0;
 
-// import "openzeppelin-s/upgrades/contracts/Initializable.sol";
+import "@openzeppelin/upgrades/contracts/Initializable.sol";
 import "./Funding.sol";
 
-contract FundingFactory {
+contract FundingFactory is Initializable {
 
+  event FundingCreated(address indexed funding, address owner, address operator, address interestToken) ;
 
-  event FundingCreated(address indexed funding, address owner, address operator) ;
+  function initialize() public initializer {
+  }
 
   function createFunding(address _cToken, address _operator) public  returns (address fundingAddress) {
 
     Funding funding = new Funding(msg.sender, _cToken, _operator);
 
     fundingAddress = address(funding);
-    emit FundingCreated(fundingAddress, msg.sender, _operator);
+    emit FundingCreated(fundingAddress, msg.sender, _operator, _cToken);
   }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,15 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('truffle-hdwallet-provider')
 // const infuraKey = "fj4jll3k.....";
 //
 // const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const mnemonic = process.env.MNEMONIC || ''
+const privateKey = process.env.PRIVATE_KEY
+const apiKey = process.env.INFURA_API_KEY
+
+const getInfuraProvider = (network) => new HDWalletProvider(privateKey || mnemonic, `https://${network}.infura.io/v3/${apiKey}`)
 
 module.exports = {
   /**
@@ -42,11 +46,11 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+      host: '127.0.0.1', // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: '*' // Any network (default: none)
+    },
 
     // Another network with more advanced options...
     // advanced: {
@@ -60,14 +64,14 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    kovan: {
+      provider: () => getInfuraProvider('kovan'),
+      network_id: 42, // Ropsten's id
+      gas: 5500000, // Ropsten has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
+    }
 
     // Useful for private networks
     // private: {
@@ -88,10 +92,10 @@ module.exports = {
       // version: '0.6.6',
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
-       optimizer: {
-         enabled: true,
-         runs: 200
-       },
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
       //  evmVersion: "byzantium"
       // }
     }
